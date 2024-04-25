2e22d85e8e667f738c8ff185fc62d2d2ace04376
Georgi Karapeev <g.karapeev@myxrobotics.com>
2024-04-25 13:05:30 +0300

    add apollo client to frontend

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import { App } from "./App";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/',
+	cache: new InMemoryCache()
+});
+
+client.query({
+	query:
+		gql`
+			query WarehousesWithProductsAndTypes {
+				warehouses {
+					id
+					name
+					products {
+						id
+						type {
+							name
+							sizePerUnit
+						}
+					}
+				}
+			}
+		`
+}).then(r => console.log(r.data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 )
