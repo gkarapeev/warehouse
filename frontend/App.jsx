a8dfe8e0778372320341ede9cbe8de561646e3ed
Georgi Karapeev <g.karapeev@myxrobotics.com>
2024-04-25 13:31:42 +0300

    move query to app component

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,4 +1,30 @@
+import { useQuery, gql } from "@apollo/client";
+import { useEffect } from "react";
+
+const GET_DATA = 
+	gql`
+		query WarehousesWithProductsAndTypes {
+			warehouses {
+				id
+				name
+				products {
+					id
+					type {
+						name
+						sizePerUnit
+					}
+				}
+			}
+		}
+	`;
+
 export const App = () => {
+	const { loading, error, data } = useQuery(GET_DATA);
+
+	useEffect(() => {
+		console.log(loading, error, data);
+	});
+
 	return (
 		<>
 			App works!
--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,34 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 import { App } from "./App";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
 	uri: 'http://localhost:4000/',
 	cache: new InMemoryCache()
 });
 
-client.query({
-	query:
-		gql`
-			query WarehousesWithProductsAndTypes {
-				warehouses {
-					id
-					name
-					products {
-						id
-						type {
-							name
-							sizePerUnit
-						}
-					}
-				}
-			}
-		`
-}).then(r => console.log(r.data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>
 )
